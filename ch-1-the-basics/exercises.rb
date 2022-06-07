932040b67e88d393a2fac7f7182644720d8e748c
Kellen James <Kellenkjames@gmail.com>
2022-06-07 11:36:31 -0400

    math operatio updates on exercise #2

--- ch-1-the-basics/exercises.rb
+++ ch-1-the-basics/exercises.rb
@@ -1,27 +1,29 @@
 # 1. Add two strings together that, when concatenated, return your first and last name as your full name in one string.
 puts "Exercise 1"
 
-full_name = "Kellen" + " James"
-puts "full name: #{full_name}"
+first_name = "Kellen"
+last_name =  "James "
+
+puts "full name: #{first_name} #{last_name}"
 
 puts "---------------------------"
 
 # 2. Use the modulo operator, division, or a combination of both to take a 4 digit number and find the digit in the: 1) thousands place 2) hundreds place 3) tens place 4) ones place
 puts "Exercise 2"
 
-four_digit_num = 6185
+four_digit_num = 4936
 
-thousands_place = four_digit_num % 10 
-hundreds_place = four_digit_num % 100 / 10
-tens_place = four_digit_num % 1000 / 100
-ones_place = four_digit_num % 10000 / 1000
+thousands_place = four_digit_num / 1000
+hundreds_place = four_digit_num % 1000 / 100
+tens_place = four_digit_num % 100 / 10
+ones_place = four_digit_num % 10
 
 puts "4 digit number: #{four_digit_num}"
 
-puts "thousands place:___#{thousands_place}"
-puts "hundrerds place:__#{hundreds_place}"
-puts "tens***** place:_#{tens_place}"
-puts "ones***** place:#{ones_place}"
+puts "thousands place:#{thousands_place}"
+puts "hundrerds place:_#{hundreds_place}"
+puts "tens***** place:__#{tens_place}"
+puts "ones***** place:___#{ones_place}"
 
 puts "---------------------------"
 
