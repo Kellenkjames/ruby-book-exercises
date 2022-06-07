e9bb096b57f67365556cefd3ff13322804cd7ad8
Kellen James <Kellenkjames@gmail.com>
2022-06-07 13:50:25 -0400

    new files added with additional problems

---
+++ ch-2-variables/age.rb
@@ -0,0 +1,15 @@
+# 2. Write a program called age.rb that asks a user how old they are and then tells them how old they will be in 10, 20, 30 and 40 years. 
+puts "How old are you?"
+age = gets.chomp.to_i     # Convert to int 
+# Your age 10 years from now
+puts "In 10 years you will be: "
+puts age + 10
+# Your age 20 years from now
+puts "In 20 years you will be: "
+puts age + 20
+# Your age 30 years from now
+puts "In 30 years you will be: "
+puts age + 30
+# Your age 40 years from now
+puts "In 40 years you will be: "
+puts age + 40
