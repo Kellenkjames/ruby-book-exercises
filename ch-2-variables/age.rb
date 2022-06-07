2f867811345b53a153315727fd26b1bd5e14d336
Kellen James <Kellenkjames@gmail.com>
2022-06-07 15:33:04 -0400

    adds solutions to age and name.rb

--- ch-2-variables/age.rb
+++ ch-2-variables/age.rb
@@ -1,15 +1,36 @@
 # 2. Write a program called age.rb that asks a user how old they are and then tells them how old they will be in 10, 20, 30 and 40 years. 
+
 puts "How old are you?"
 age = gets.chomp.to_i     # Convert to int 
+
 # Your age 10 years from now
 puts "In 10 years you will be: "
 puts age + 10
+
 # Your age 20 years from now
 puts "In 20 years you will be: "
 puts age + 20
+
 # Your age 30 years from now
 puts "In 30 years you will be: "
 puts age + 30
+
 # Your age 40 years from now
 puts "In 40 years you will be: "
 puts age + 40
+
+puts "-----------------------------"
+
+#5. Look at the following programs... 
+x = 0
+3.times do 
+  x += 1
+end 
+puts x  # Expect 3
+
+y = 0
+3.times do 
+  y += 1
+  x = y
+end 
+puts a # Expect 3
--- ch-2-variables/name.rb
+++ ch-2-variables/name.rb
@@ -4,6 +4,18 @@ name = gets.chomp
 
 puts "Hello #{name}, we hope you have a great day! "
 
+# 3. Prints the name of the user 10 times. 
+10.times { puts name }
+
+#4. Modify name.rb again so that it first asks the user for their first name, saves it into a variable, and then does the same for the last name. Then outputs their full name all at once. 
+puts "What is your first name?"
+first_name = gets.chomp 
+
+puts "What is your last name?"
+last_name = gets.chomp 
+
+puts "Full Name: #{first_name} #{last_name}"
+
 
 
 
