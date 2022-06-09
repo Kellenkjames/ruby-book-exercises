df387bcd7fa22e7d96daa2af6493c6b5a6bf4a7b
Kellen James <Kellenkjames@gmail.com>
2022-06-09 16:09:22 -0400

    completes exercises for ch-5 loops + recursion

--- ch-5-loops/exercises.rb
+++ ch-5-loops/exercises.rb
@@ -1,19 +1,42 @@
-#1. What does the "each" method in the following program return after it is finished executing? 
+# exercises.rb 
+
+# 1. What does the "each" method in the following program return after it's finished executing? 
 puts "Exercise 1"
 
 x = [1, 2, 3, 4, 5]
-x.each do |a|
-  p a + 1
+x.each do |a| 
+  a + 1 
 end 
 
-# Answer: 2, 3, 4, 5, 6
+# Answer: Adds one to each index... 2, 3, 4, 5, 6
 
-p "------------------------"
-
-#2. Write a while loop that takes input from the user, performs an action, and only stops when the user types "STOP". Each loop can get info from the user. 
+# 2. Write a while loop that takes input from the user, performs an action, and only stops when the user types "STOP". Each loop can get info from the user. 
 puts "Exercise 2"
 
+p "-----------------------------------------------------------"
 
+y = 0 
 
+while y <= 5 
+  x = gets.chomp.to_s.upcase
+  if x == 'STOP'
+    break 
+  end 
+  puts "Please enter stop to end loop, otherwise keep going. "
+  y += 1
+end
 
+p "-----------------------------------------------------------"
+
+# 3. Write a method that counts down to zero using recursion
+puts "Exercise 3"
+
+def countdown(number)
+  puts number 
+  if number > 0
+    countdown(number - 1)
+  end 
+end
+
+countdown(12)
 
--- ch-5-loops/fibonacci.rb
+++ ch-5-loops/fibonacci.rb
@@ -1,4 +1,5 @@
 # fibonacci.rb
+
 def fibonacci(number)
   if number < 2
     number
@@ -9,4 +10,3 @@ end
 
 puts fibonacci(6)
 
-# Key Concept: There is some baseline condition that returns a "value", when then "unwinds" the recursive calls. You can think of the sucessive recursive calls building up, until some value is returned, and only then can the recursive calls be evaluated. 
