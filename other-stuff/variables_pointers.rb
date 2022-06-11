7cac46b733d84278a7ec0048d7a42a3602d72cf4
Kellen James <Kellenkjames@gmail.com>
2022-06-11 17:43:50 -0400

    creates folder and files for Ruby other-stuff

---
+++ other-stuff/variables_pointers.rb
@@ -0,0 +1,46 @@
+# variables_pointers.rb 
+puts "Example 1"
+
+# Note: The major point of this section: some operations mutate the address space, while others simply make the variable point to a different address space.
+
+# Always pay attention to whether your variables are pointing to the same object (address space) or if they are dealing with copies that occupy different address spaces. 
+
+a = "hi there"
+b = a # b is pointing to the value stored in "a"
+a = "not here"
+
+p b 
+p a 
+
+puts "---------------------------------------"
+
+puts "Example 2"
+
+c = "hi there"
+d = c
+c <<", Bob" # mutates the caller "c"
+
+puts c
+
+puts "---------------------------------------"
+
+puts "Example 3"
+
+a = [1, 2, 3, 3]
+b = a # b is pointing to the address space of "a"
+c = a.uniq # c is pointing to the address space of "a" - and makes a copy  (non-destructive)
+
+p a
+p b
+p c
+
+puts "---------------------------------------"
+
+puts "Example 4"
+
+def test(b)
+  b.map! { |letter| "I like the letter: #{letter}"}
+end 
+
+a = ['a', 'b', 'c']
+puts test(a)
